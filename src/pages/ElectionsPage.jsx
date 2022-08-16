9f66eefe264259cf346bf95fd8efc86f76476475
thiagokassabian <thiago.kassabian@gmail.com>
2022-08-16 11:24:52 -0300

    removendo importações desnecessárias

--- src/pages/ElectionsPage.jsx
+++ src/pages/ElectionsPage.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useLayoutEffect, useRef, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Container } from 'react-bootstrap';
 import Election from '../components/Election';
 import Header from '../components/Header';
