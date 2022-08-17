9718ff923d8b3ad33938be36990933ae616997dd
thiagokassabian <thiago.kassabian@gmail.com>
2022-08-17 17:26:19 -0300

    Adicionado endereço de backend na nuvem

--- src/services/httpService.js
+++ src/services/httpService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = process.env.NODE_ENV === 'development' ? 'http://localhost:3001' : '';
+const BASE_URL = process.env.NODE_ENV === 'development' ? 'http://localhost:3001' : 'https://elections-backend-tkassabian.glitch.me';
 
 const axiosInstance = axios.create({ baseURL: BASE_URL });
 
